ba2f25c1740a8a6c68fe0170ed0d3c6877c8f7ee
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-10-10 05:33:27 +0000

    set responsive navbar

--- nomada/src/App.js
+++ nomada/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Wasaaaaaaaaaaaaaaaaaaaaaap</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+
+    </Router>
+    </>
   );
 }
 
